cb4d110c71caed81fc09966513231bf6aedcf36c
Jason Perrins <jmpdev@outlook.com>
2020-10-01 20:58:17 +0100

    Adding version property and display to allow to test rolling back and forward versions

--- src/main/java/com/perrinsjason/demo/greetingservice/controller/HelloWorldController.java
+++ src/main/java/com/perrinsjason/demo/greetingservice/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.perrinsjason.demo.greetingservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+
+
     @GetMapping("/")
-    public String helloWorld(){
-        return "Hello World!";
+    public String helloWorld( @Value("${application.version}") String appVersion){
+        return "Hello World! "+appVersion;
     }
 }
